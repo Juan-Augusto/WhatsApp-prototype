2105b11290069c8de33eb6229790147f2ec10f0a
juan-augusto <juanaugusto1@live.com>
2022-03-07 07:51:43 -0300

    Firebase settings

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCgdPfoeACIYaQI52ZKh7zdz9T-Ygi0ac",
+    authDomain: "whatsappclone-juanaugusto.firebaseapp.com",
+    projectId: "whatsappclone-juanaugusto",
+    storageBucket: "whatsappclone-juanaugusto.appspot.com",
+    messagingSenderId: "405572101111",
+    appId: "1:405572101111:web:9875805ca54e0080da44f2"
+};
+export default firebaseConfig;
